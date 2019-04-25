b906839fea9729a5a15018375625f02068140753
hcgustavo <ghcaixeta@gmail.com>
2019-04-25 07:31:04 -0300

    port config

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const questions = require('./questions');
 
+let port = process.env.PORT || 8080;
+
 app.get('/', (req, res, next) => {
     res.send(`
     <p>ConjugApp API</p>
@@ -13,6 +15,6 @@ app.get('/api/questions', (req, res, next) => {
     res.status(200).json(questions);
 });
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 })
